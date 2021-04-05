34d3f85d1a5844f736a5e87b6c7b5f98a786fc5a
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-04-05 20:43:48 +0200

    feat: single point

--- src/main/java/PointSET.java
+++ src/main/java/PointSET.java
@@ -1,37 +1,59 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
 
+import java.util.LinkedList;
+
+//Throw an IllegalArgumentException if any argument is null
 public class PointSET {
+    private final SET<Point2D> point2DSet;
+
     // construct an empty set of points
     public PointSET() {
+        point2DSet = new SET<>();
     }
 
     public boolean isEmpty() {
-        return true;
+        return point2DSet.isEmpty();
     }
 
     public int size() {
-        return 0;
+        return point2DSet.size();
     }
 
     public void insert(Point2D p) {
+        point2DSet.add(p);
     }
 
     public boolean contains(Point2D p) {
-        return false;
+        return point2DSet.contains(p);
     }
 
     // draw all points to standard draw
     public void draw() {
+        point2DSet.iterator().forEachRemaining(Point2D::draw);
     }
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        LinkedList<Point2D> points = new LinkedList<>();
+        point2DSet.iterator().forEachRemaining(p -> {
+            if (rect.contains(p)) points.addLast(p);
+        });
+        return points;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-        return p;
+        double minDistance = 1;
+        Point2D champion = null;
+        for (Point2D point : point2DSet) {
+            double distanceSquaredTo = point.distanceSquaredTo(p);
+            if (distanceSquaredTo < minDistance) {
+                minDistance = distanceSquaredTo;
+                champion = point;
+            }
+        }
+        return champion;
     }
 }
--- src/test/java/PointSETTest.java
+++ src/test/java/PointSETTest.java
@@ -1,13 +1,35 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PointSETTest {
 
+    private static final RectHV TOTAL_AREA = new RectHV(0, 0, 1, 1);
+
     @Test
     public void empty_point_set() {
         PointSET testee = new PointSET();
         assertThat(testee.isEmpty()).isTrue();
         assertThat(testee.size()).isEqualTo(0);
+        assertThat(testee.nearest(new Point2D(0.1d, 0.2d))).isNull();
+        assertThat(testee.range(TOTAL_AREA)).isEmpty();
     }
+
+    @Test
+    public void one_element_point_set() {
+        PointSET testee = new PointSET();
+        testee.insert(new Point2D(0.1d, 0.2d));
+
+        assertThat(testee.isEmpty()).isFalse();
+        assertThat(testee.size()).isEqualTo(1);
+        assertThat(testee.contains(new Point2D(0.1d, 0.2d))).isTrue();
+        assertThat(testee.nearest(new Point2D(0.3d, 0.6d))).isEqualByComparingTo(new Point2D(0.1d, 0.2d));
+        assertThat(testee.range(TOTAL_AREA)).hasSize(1).containsOnly(new Point2D(0.1d, 0.2d));
+        assertThat(testee.range(new RectHV(0.3, 0.3, 0.5, 0.6))).isEmpty();
+    }
+
+    //search in rectangle
+
 }
